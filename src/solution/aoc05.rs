6bcb12358a3a9d5aa35ef12e1900aa6be5da79b5
thanakritju <thanakrit.ju.work@gmail.com>
2023-08-01 22:27:32 +0700

    day5 part 1

---
+++ src/solution/aoc05.rs
@@ -0,0 +1,108 @@
+use std::collections::HashMap;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn aoc_05_part1(strs: &str) -> i32 {
+        let mut count = 0;
+        for line in strs.lines() {
+            if !Solution::is_forbidden(line)
+                && Solution::same_letter(line)
+                && Solution::exceeds_vowels(line)
+            {
+                count += 1
+            }
+        }
+        count
+    }
+
+    fn same_letter(s: &str) -> bool {
+        let ans = s
+            .chars()
+            .collect::<Vec<char>>()
+            .windows(2)
+            .any(|x| x[0] == x[1]);
+        ans
+    }
+
+    fn is_forbidden(s: &str) -> bool {
+        let forbidden_list = ["ab", "cd", "pq", "xy"];
+        let ans = s.chars().collect::<Vec<char>>().windows(2).any(|y| {
+            forbidden_list
+                .iter()
+                .position(|&x| x == format!("{}{}", y[0], y[1]))
+                .is_some()
+        });
+        ans
+    }
+
+    fn exceeds_vowels(s: &str) -> bool {
+        let vowels = "aeiou";
+        let mut vowels_count = 0;
+        for c in s.chars().collect::<Vec<char>>() {
+            if vowels.chars().position(|x| x == c).is_some() {
+                vowels_count += 1
+            }
+        }
+        vowels_count >= 3
+    }
+
+    pub fn aoc_05_part2(strs: &str) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn test_is_forbidden() {
+        assert_eq!(true, Solution::is_forbidden("haegwjzuvuyypxy"));
+        assert_eq!(true, Solution::is_forbidden("xyhaegwjzuvuyyp"));
+        assert_eq!(false, Solution::is_forbidden("pa"));
+    }
+
+    #[test]
+    fn test_same_letter() {
+        assert_eq!(false, Solution::same_letter("abc"));
+        assert_eq!(true, Solution::same_letter("aa"));
+        assert_eq!(false, Solution::same_letter("pa"));
+    }
+
+    #[test]
+    fn test_05_part1() {
+        assert_eq!(1, Solution::aoc_05_part1("ugknbfddgicrmopn"));
+        assert_eq!(4, Solution::aoc_05_part1("aaa\naaa\naaa\naaa"));
+        assert_eq!(1, Solution::aoc_05_part1("aaccbbdde"));
+        assert_eq!(0, Solution::aoc_05_part1("aaab"));
+        assert_eq!(1, Solution::aoc_05_part1("iii"));
+        assert_eq!(0, Solution::aoc_05_part1("xazegov"));
+        assert_eq!(0, Solution::aoc_05_part1("jchzalrnumimnmhp"));
+        assert_eq!(0, Solution::aoc_05_part1("haegwjzuvuyypxy"));
+        assert_eq!(0, Solution::aoc_05_part1("dvszwmarrgswjxmb"));
+        assert_eq!(
+            1,
+            Solution::aoc_05_part1("jchzalrnumimnmhp\naaa\nhaegwjzuvuyypxyu\ndvszwmarrgswjxmb\n")
+        );
+    }
+
+    #[test]
+    fn test_05_part2() {
+        assert_eq!(0, Solution::aoc_05_part2("(())"));
+    }
+
+    #[test]
+    fn test_05_part1_puzzle() {
+        let data = fs::read_to_string("src/solution/input05.txt").expect("Unable to read file");
+        assert_eq!(255, Solution::aoc_05_part1(&data));
+    }
+
+    #[test]
+    fn test_05_part2_puzzle() {
+        let data = fs::read_to_string("src/solution/input05.txt").expect("Unable to read file");
+        assert_eq!(0, Solution::aoc_05_part2(&data));
+    }
+}
--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -2,3 +2,4 @@ mod aoc01;
 mod aoc02;
 mod aoc03;
 mod aoc04;
+mod aoc05;
